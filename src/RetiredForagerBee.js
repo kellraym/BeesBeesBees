f9a4308b9a37acc4540218329187f8ba3ab78522
kellraym <83917084+kellraym@users.noreply.github.com>
2021-05-19 20:44:06 -0600

    Add RetiredForagerBee class

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,17 @@
-/* START SOLUTION */
+class RetiredForagerBee extends ForagerBee{
+  constructor() {
+    super()
+      this.age = 40,
+      this.job = `gamble`,
+      this.canFly = false,
+      this.color = `grey`
+    }
 
-/* END SOLUTION */
+    forage() {
+      return `I am too old, let me play cards instead`;
+    }
+
+    gamble(treasure) {
+      this.treasureChest.push(treasure);
+    } 
+}
